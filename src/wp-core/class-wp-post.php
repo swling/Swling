5a5a85c28c852abca95243b1731ebe990f2c2696
Swling <tangfou@gmail.com>
2022-02-25 10:45:09 +0800

    重构 WP：WP_Post

---
+++ src/wp-core/class-wp-post.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * Post API: WP_Post class
+ *
+ * @package WordPress
+ * @subpackage Post
+ * @since 4.4.0
+ */
+
+/**
+ * Core class used to implement the WP_Post object.
+ *
+ * @since 3.5.0
+ *
+ * @property string $page_template
+ *
+ * @property-read int[]  $ancestors
+ * @property-read int    $post_category
+ * @property-read string $tag_input
+ */
+final class WP_Post {
+
+	/**
+	 * Post ID.
+	 *
+	 * @since 3.5.0
+	 * @var int
+	 */
+	public $ID;
+
+	/**
+	 * ID of post author.
+	 *
+	 * A numeric string, for compatibility reasons.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_author = 0;
+
+	/**
+	 * The post's local publication time.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_date = '1970-01-01 08:00:00';
+
+	/**
+	 * The post's GMT publication time.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_date_gmt = '1970-01-01 08:00:00';
+
+	/**
+	 * The post's content.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_content = '';
+
+	/**
+	 * The post's title.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_title = '';
+
+	/**
+	 * The post's excerpt.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_excerpt = '';
+
+	/**
+	 * The post's status.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_status = 'publish';
+
+	/**
+	 * Whether comments are allowed.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $comment_status = 'open';
+
+	/**
+	 * Whether pings are allowed.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $ping_status = 'open';
+
+	/**
+	 * The post's password in plain text.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_password = '';
+
+	/**
+	 * The post's slug.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_name = '';
+
+	/**
+	 * URLs queued to be pinged.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $to_ping = '';
+
+	/**
+	 * URLs that have been pinged.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $pinged = '';
+
+	/**
+	 * The post's local modified time.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_modified = '1970-01-01 08:00:00';
+
+	/**
+	 * The post's GMT modified time.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_modified_gmt = '1970-01-01 08:00:00';
+
+	/**
+	 * A utility DB field for post content.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_content_filtered = '';
+
+	/**
+	 * ID of a post's parent post.
+	 *
+	 * @since 3.5.0
+	 * @var int
+	 */
+	public $post_parent = 0;
+
+	/**
+	 * The unique identifier for a post, not necessarily a URL, used as the feed GUID.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $guid = '';
+
+	/**
+	 * A field used for ordering posts.
+	 *
+	 * @since 3.5.0
+	 * @var int
+	 */
+	public $menu_order = 0;
+
+	/**
+	 * The post's type, like post or page.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_type = 'post';
+
+	/**
+	 * An attachment's mime type.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $post_mime_type = '';
+
+	/**
+	 * Cached comment count.
+	 *
+	 * A numeric string, for compatibility reasons.
+	 *
+	 * @since 3.5.0
+	 * @var string
+	 */
+	public $comment_count = 0;
+
+	/**
+	 * Retrieve WP_Post instance.
+	 *
+	 * @since 3.5.0
+	 *
+	 * @global wpdb $wpdb WordPress database abstraction object.
+	 *
+	 * @param int $post_id Post ID.
+	 * @return WP_Post|false Post object, false otherwise.
+	 */
+	public static function get_instance(int $post_id): object{
+		$handler = new Model\WPDB_Handler_Post;
+		$post    = $handler->get($post_id);
+		return new WP_Post($post);
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.5.0
+	 *
+	 * @param WP_Post|object $post Post object.
+	 */
+	public function __construct(object $post) {
+		foreach (get_object_vars($post) as $key => $value) {
+			$this->$key = $value;
+		}
+	}
+
+	/**
+	 * Convert object to array.
+	 *
+	 * @since 3.5.0
+	 *
+	 * @return array Object as array.
+	 */
+	public function to_array(): array{
+		$post = get_object_vars($this);
+
+		return $post;
+	}
+}
