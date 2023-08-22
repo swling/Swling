917d7ccdfa0f03dacad24085964f5a4284f320ef
Swling <tangfou@gmail.com>
2023-08-22 12:33:03 +0800

    init

--- content/themes/default/single.php
+++ content/themes/default/single.php
@@ -8,12 +8,15 @@ get_header();
 
 while (have_posts()) : the_post();
 	global $post;
+	echo '<h1>' . $post->post_title . '</h1>';
 	echo $post->post_content . '<br/>';
 
 	$categories = wp_get_object_terms($post->ID, 'category');
 	foreach ($categories as $category) {
 		echo '<a href="' . get_term_link($category) . '">' . $category->name . '</a>';
 	}
+
+// print_r($post);
 endwhile;
 
 ?>
