434bf0debc19f9ec4abd11a6121e89b4108bfd87
Swling <tangfou@gmail.com>
2023-08-22 12:36:14 +0800

    init

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -8,7 +8,7 @@ print_r(get_queried_object());
 // global $wp_query;
 // print_r($wp_query);
 
-while (have_posts()) : the_post();
+while (have_posts()): the_post();
 	global $post;
 	echo '<a href="' . get_permalink() . '">' . $post->post_title . '</a><br/>';
 endwhile;
@@ -17,9 +17,13 @@ endwhile;
 <h1><?php echo basename(__FILE__); ?></h1>
 
 <script>
-	wnd_ajax_modal("user/wnd_profile_form", {
-		"haha": "1016",
-		"caobi": "好"
+	// wnd_ajax_modal("user/wnd_profile_form", {
+	// 	"haha": "1016",
+	// 	"caobi": "好"
+	// });
+
+	wnd_ajax_modal("post/wnd_post_form_post", {
+		"post_id": 2517,
 	});
 </script>
 
--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
 require 'config.php';
 
+// $handler = WP_Core\Model\WPDB_Handler_Meta::get_instance();
+// $handler->set_meta_type('post');
+// print_r($handler->get_rows(1));
+// print_r($handler->get_meta(1, 'views'));
+// print_r($handler->get_meta(1, 'nickname'));
+// var_dump($handler->add_meta(1, 'nickname', time()));
+// var_dump($handler->update_meta(1, 'nickname', time()));
+// print_r($handler->get_meta(1, 'nickname'));
+// print_r($handler->delete_meta(1, 'nickname'));
+// exit;
+
 $route = new Controller\Route();
 $route->dispatch();
 return;
 
+// var_dump(wp_update_post(['ID' => 1, 'post_title' => time()]));
+// print_r(get_post(1));
+// delete_post_meta(1, 'test');
+// get_post_meta(1, 'test');
+// get_post_meta(1, 'test');
+// echo get_post_meta(1, 'test');
+
 ## Test Code Area
+// $wpdb_handler = WP_Core\Model\WPDB_Handler_Post::get_instance();
+// echo $wpdb_handler->insert(['post_title' => time(), 'post_type' => 'post', 'post_name' => uniqid()]);
+// echo $wpdb_handler->update(['post_title' => time(), 'ID' => '2525']);
+// print_r($wpdb_handler->get(2525));
+// print_r($wpdb_handler->delete(['ID' => '2525']));
 
 ## Query
 global $wpdb;
